0f5ea5bae15dd149e58fae501604b933c6543600
CristinaRoberto <cristina.roberto@estudiantat.upc.edu>
2021-11-12 09:40:32 +0100

    Version 2

---
+++ MISERVIDOR.c
@@ -0,0 +1,584 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <mysql.h>
+#include <pthread.h>
+
+typedef struct {
+	char nombre[20];
+	
+}Conectado;
+
+typedef struct {
+	Conectado conectados[100];
+	int num;
+}ListaConectados;
+
+int Pon (ListaConectados *lista, char nombre[20])
+{
+	if(lista->num == 100)
+		return -1;
+	else{
+		strcpy(lista->conectados[lista->num].nombre, nombre);
+		
+		lista->num++;
+		return 0;
+		
+	}
+	
+	
+}
+int DamePosicion (ListaConectados *lista, char nombre[20]){
+	int i = 0;
+	int encontrado = 0;
+	while((i < lista->num) && !encontrado)
+	{
+		if( strcmp(lista->conectados[i].nombre,nombre) == 0){
+			encontrado = 1;
+		}
+		if ( !encontrado){
+			
+			i=i+1;
+		}
+		
+	}
+	if (encontrado)
+		return i;
+	else
+		return -1;
+}
+
+int Eliminar(ListaConectados *lista,char nombre[20])
+{
+	int pos = DamePosicion(lista,nombre);
+	if(pos == -1)
+		return -1;
+	else{
+		int i = 0;
+		for(i = pos; i < (lista->num-1);i++)
+		{
+			lista->conectados[i] = lista->conectados[i+1];
+			strcpy(lista->conectados[i].nombre, lista->conectados[i+1].nombre);
+			
+		}
+		lista->num--;
+		return 0;
+	}
+	
+	
+}
+void DameConectados (ListaConectados *lista, char conectados[300]) {
+	sprintf(conectados,"%d",lista->num);
+	int i;
+	for(i=0;i<lista->num;i++)
+	{
+		sprintf(conectados,"%s/%s", conectados, lista->conectados[i].nombre);
+	}
+	
+}
+
+ListaConectados milista;
+
+
+
+
+
+void *AtenderCliente (void *socket)
+{
+	int sock_conn;
+	int *s;
+	s = (int *) socket;
+	sock_conn = *s;
+	
+	char buff[512];
+	char buff2[512];
+	char buff3[512];
+	int ret;
+	
+	int terminar = 0;
+	while(terminar==0){
+		// Ahora recibimos su nombre, que dejamos en buff
+		ret=read(sock_conn,buff, sizeof(buff));
+		printf ("Recibido\n");
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		buff[ret]='\0';
+		
+		//Escribimos el nombre en la consola
+		
+		printf ("Se ha conectado: %s\n",buff);
+		
+		
+		char *p = strtok( buff, "/");
+		int codigo =  atoi (p);
+		if (codigo == 7){
+			p = strtok( NULL, "/");
+			char nombre[20];
+			strcpy(nombre,p);
+			Eliminar(&milista,nombre);
+		}
+		if (codigo == 6){
+			
+			DameConectados(&milista,buff2);
+			write (sock_conn,buff2, strlen(buff2));
+		}
+		if((codigo == 1) ||(codigo == 2)){
+			p = strtok( NULL, "/");
+			char nombre[20];
+			strcpy (nombre, p);
+			p = strtok( NULL, "/");
+			char contrasena[20];
+			strcpy(contrasena,p);
+			
+			printf ("Codigo: %d, Nombre: %s, Contraseña: %s\n", codigo, nombre, contrasena);
+			
+			if (codigo == 1)//Codigo de acceso
+			{
+				printf("hola");
+				MYSQL *conn;
+				int err;
+				
+				// Estructura especial para almacenar resultados de consultas
+				MYSQL_RES *resultado;
+				MYSQL_ROW row;
+				
+				int ID;
+				char consulta [80];
+				char consulta2 [80];
+				char fecha[20];
+				char hora[20];
+				//Creamos una conexion al servidor MYSQL
+				conn = mysql_init(NULL);
+				if (conn==NULL) 
+				{
+					printf ("Error al crear la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				//inicializar la conexion, indicando nuestras claves de acceso al servidor de BBDD
+				conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego", 0, NULL,0);
+				if (conn==NULL)
+				{
+					printf ("Error al inicializar la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}	
+				sprintf (consulta2,"SELECT nombre FROM jugadores WHERE nombre ='%s' and contraseña = '%s' ",nombre,contrasena);
+				
+				err=mysql_query (conn, consulta2);
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				//recogemos el resultado de la consulta
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				
+				if (row == NULL){
+					
+					printf ("No se han obtenido datos en la consulta\n");
+					strcpy(buff2,"NO,");
+					write (sock_conn,buff2, strlen(buff2));
+					
+				}
+				else{
+					while(row!=NULL){
+						
+						// El resultado debe ser una matriz con una sola fila
+						// y una columna que contiene el nombre
+						//printf ("El ganador es: %s\n", row[3] );
+						printf ("El usuario es %s\n", row[0]);
+						row = mysql_fetch_row (resultado);
+						
+						
+					}
+					strcpy(buff2,"SI,");
+					Pon(&milista,nombre);
+					write (sock_conn,buff2, strlen(buff2));
+					
+				}
+				
+				
+				
+			}	
+			
+			//sprintf (buff2,"%d,",strlen (nombre));
+			
+			
+			
+			else if(codigo == 2){
+				// Código de registro
+				MYSQL *conn;
+				int err;
+				
+				// Estructura especial para almacenar resultados de consultas
+				MYSQL_RES *resultado;
+				MYSQL_ROW row;
+				
+				int ID;
+				char consulta1 [80];
+				char consulta2 [80];
+				char fecha[20];
+				char hora[20];
+				//Creamos una conexion al servidor MYSQL
+				conn = mysql_init(NULL);
+				if (conn==NULL) 
+				{
+					printf ("Error al crear la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				//inicializar la conexion, indicando nuestras claves de acceso al servidor de BBDD
+				conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego", 0, NULL,0);
+				if (conn==NULL)
+				{
+					printf ("Error al inicializar la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}	
+				sprintf (consulta1,"SELECT nombre FROM jugadores WHERE nombre ='%s' and contraseña = '%s' ",nombre,contrasena);
+				sprintf (consulta2,"INSERT INTO jugadores values('%s','%s') ",nombre,contrasena);
+				
+				err=mysql_query (conn, consulta1);
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				//recogemos el resultado de la consulta
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				
+				if (row == NULL){
+					
+					mysql_query (conn, consulta2);
+					printf ("Registrado\n");
+					strcpy(buff2,"SI,");
+					write (sock_conn,buff2, strlen(buff2));
+					
+				}
+				else{
+					
+					strcpy(buff2,"NO,");
+					write (sock_conn,buff2, strlen(buff2));
+				}
+				
+				
+				
+			}	
+		}
+		else if((codigo == 4) || (codigo ==5)){
+			p = strtok( NULL, "/");
+			char nombre[20];
+			strcpy (nombre, p);
+			
+			
+			
+			if (codigo == 4)
+			{
+				printf("%s",buff);
+				MYSQL *conn;
+				int err;
+				
+				// Estructura especial para almacenar resultados de consultas
+				MYSQL_RES *resultado;
+				MYSQL_ROW row;
+				//char nombre[20];
+				int ID;
+				char consulta4 [80];
+				//Creamos una conexion al servidor MYSQL
+				conn = mysql_init(NULL);
+				if (conn==NULL) 
+				{
+					printf ("Error al crear la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				//inicializar la conexion, indicando nuestras claves de acceso al servidor de BBDD
+				conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego", 0, NULL,0);
+				if (conn==NULL)
+				{
+					printf ("Error al inicializar la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				
+				// Ahora vamos a buscar el nombre de la persona cuyo nombre es uno
+				// dado por el usuario
+				printf ("Dame el nombre del usuario:\n");
+				
+				// construimos la consulta SQL
+				strcpy (consulta4,"SELECT SUM(puntuacion.puntos) FROM jugadores,puntuacion WHERE jugadores.nombre = '");
+				strcat (consulta4, nombre);
+				strcat (consulta4, "'AND jugadores.nombre = puntuacion.jugador");
+				
+				// hacemos la consulta
+				err=mysql_query (conn, consulta4);
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				//recogemos el resultado de la consulta
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				
+				if (row == NULL){
+					printf ("No se han obtenido datos en la consulta\n");
+					strcpy(buff2,"NO,");
+					write (sock_conn,buff2, strlen(buff2));
+				}
+				else{
+					// El resultado debe ser una matriz con una sola fila
+					// y una columna que contiene el nombre
+					printf ("La suma total de puntos es: %s\n", row[0] );
+					sprintf(buff2,"%s",row[0]);
+					write (sock_conn,buff2, strlen(buff2));
+					mysql_close (conn);
+					
+					
+				}
+				
+				
+				// cerrar la conexion con el servidor MYSQL
+				
+				
+				
+			}
+			
+			
+			else if (codigo == 5)
+			{
+				MYSQL *conn;
+				int err;
+				
+				// Estructura especial para almacenar resultados de consultas
+				MYSQL_RES *resultado;
+				MYSQL_ROW row;
+				
+				int ID;
+				char consulta5 [80];
+				//Creamos una conexion al servidor MYSQL
+				conn = mysql_init(NULL);
+				if (conn==NULL) 
+				{
+					printf ("Error al crear la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				//inicializar la conexion, indicando nuestras claves de acceso al servidor de BBDD
+				conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego", 0, NULL,0);
+				if (conn==NULL)
+				{
+					printf ("Error al inicializar la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				
+				// Ahora vamos a buscar el nombre de la persona cuyo nombre es uno
+				// dado por el usuario
+				
+				// construimos la consulta SQL
+				strcpy (consulta5,"SELECT partidas.fecha_hora FROM jugadores,partidas WHERE jugadores.nombre = '");
+				strcat (consulta5, nombre);
+				strcat (consulta5, "'AND jugadores.nombre = partidas.ganador");
+				
+				// hacemos la consulta
+				err=mysql_query (conn, consulta5);
+				if (err!=0) {
+					printf ("Error al consultar datos de la base %u %s\n",
+							mysql_errno(conn), mysql_error(conn));
+					
+					exit (1);
+				}
+				
+				//recogemos el resultado de la consulta
+				resultado = mysql_store_result (conn);
+				row = mysql_fetch_row (resultado);
+				
+				if (row == NULL)
+					printf ("No se han obtenido datos en la consulta\n");
+				else{
+					char salida[500];
+					salida[0]='\0';
+					while (row !=NULL) {
+						// El resultado debe ser una matriz con una sola fila
+						// y una columna que contiene el nombre
+						//sprintf(salida,"%s %s",salida,row[0]);
+						printf("contenido salida:%s\n",salida);
+						printf("row0:%s\n",row[0]);
+						sprintf(salida,"%s %s ",salida,row[0]);
+						printf("nuevo valor de salida:%s\n",salida);
+						printf ("%s jugo una partida el dia y hora: %s\n",nombre, row[0]);
+						row = mysql_fetch_row (resultado);
+					}
+					printf("La salida definitiva es:%s\n",salida);
+					sprintf(buff3,"%s",salida);
+					printf("Envio la siguiente respuesta:%s\n",buff3);
+					write (sock_conn,buff3, strlen(buff3));
+					
+				}
+				
+				// cerrar la conexion con el servidor MYSQL
+				mysql_close (conn);
+				//exit(0);
+				
+				
+			}
+		}
+		else if (codigo == 3)
+		{
+			p = strtok( NULL, "/");
+			char fechayhora[60];
+			strcpy (fechayhora, p);
+			
+			MYSQL *conn;
+			int err;
+			
+			
+			// Estructura especial para almacenar resultados de consultas
+			MYSQL_RES *resultado;
+			MYSQL_ROW row;
+			
+			int ID;
+			char consulta [200];
+			
+			//Creamos una conexion al servidor MYSQL
+			conn = mysql_init(NULL);
+			if (conn==NULL) 
+			{
+				printf ("Error al crear la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+				
+				exit (1);
+			}
+			//inicializar la conexion, indicando nuestras claves de acceso al servidor de BBDD
+			conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego", 0, NULL,0);
+			if (conn==NULL)
+			{
+				printf ("Error al inicializar la conexion: %u %s\n", mysql_errno(conn), mysql_error(conn));
+				
+				exit (1);
+			}
+			
+			
+			// Ahora vamos a buscar el nombre de la persona cuyo nombre es uno
+			// dado por el usuario
+			
+			
+			// construimos la consulta SQL
+			strcpy (consulta,"SELECT * FROM partidas WHERE fecha_hora = '");
+			strcat (consulta, fechayhora);
+			
+			strcat (consulta, "'");	
+			printf("%s",consulta);
+			// hacemos la consulta
+			err=mysql_query (conn, consulta);
+			if (err!=0) {
+				printf ("Error al consultar datos de la base %u %s\n",
+						mysql_errno(conn), mysql_error(conn));
+				
+				exit (1);
+			}
+			
+			//recogemos el resultado de la consulta
+			resultado = mysql_store_result (conn);
+			row = mysql_fetch_row (resultado);
+			
+			if (row == NULL)
+				printf ("No se han obtenido datos en la consulta\n");
+			else{
+				char salida[50];
+				salida[0]='\0';
+				while(row!=NULL){
+					
+					// El resultado debe ser una matriz con una sola fila
+					// y una columna que contiene el nombre
+					printf ("El ganador es: %s\n", row[3] );
+					sprintf(salida,"%s %s",salida,row[3]);
+					
+					row = mysql_fetch_row (resultado);
+				}
+				sprintf(buff2,"%s",salida);
+				write (sock_conn,buff2, strlen(buff2));
+			}
+			// cerrar la conexion con el servidor MYSQL
+			mysql_close (conn);
+			//exit(0);
+			
+			
+		}
+	}
+	close(sock_conn);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	
+	int sock_conn, sock_listen;
+	struct sockaddr_in serv_adr;
+	
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// escucharemos en el port 9050
+	serv_adr.sin_port = htons(9000);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	//La cola de peticiones pendientes no podr? ser superior a 4
+	if (listen(sock_listen, 2) < 0)
+		printf("Error en el Listen");
+	
+	
+	
+	
+	
+	
+	
+	
+	int i;
+	int sockets[100];
+	pthread_t thread[100];
+	
+	
+    for(i=0;i<2000;i++){
+	printf ("Escuchando\n");
+	
+	sock_conn = accept(sock_listen, NULL, NULL);
+	printf ("He recibido conexi?n\n");
+	sockets[i] = sock_conn;
+	char nombre[20];
+	//sock_conn es el socket que usaremos para este cliente
+	pthread_create(&thread[i], NULL, AtenderCliente,&sockets[i]);
+	
+		
+	}
+	//close(sock_conn); 
+	
+}
+
---
+++ Version2/WindowsFormsApplication1/Entrar.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Entrar : Form
+    {
+        Socket server;
+        public Entrar()
+        {
+            InitializeComponent();
+        }
+        public void setServidor(Socket Servidor)
+        {
+            this.server = Servidor;
+        
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9000);
+            
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Show();
+            textBox3.Show();
+            label4.Show();
+            textBox4.Show();
+            button4.Show();
+            button6.Show();
+
+            button3.Hide();
+            label1.Hide();
+            textBox1.Hide();
+            label2.Hide();
+            textBox2.Hide();
+            button2.Hide();
+            button1.Hide();
+
+            
+
+            
+            
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mensaje = "1/" + textBox1.Text + "/" + textBox2.Text + "";
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+           
+
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+            if (mensaje == "SI")
+            {
+                MessageBox.Show("Acceso a la aplicación");
+                textBox5.Show();
+                radioButton1.Show();
+                 radioButton2.Show();
+                 radioButton3.Show();
+                 button5.Show();
+                 button7.Show();
+                 button8.Show();
+                
+
+                 button3.Hide();
+                 label3.Hide();
+                label1.Hide();
+                 textBox1.Hide();
+                label2.Hide();
+                 textBox2.Hide();
+                 button2.Hide();
+                  button1.Hide();
+                
+                
+               
+
+            }
+            else if (mensaje == "NO")
+            {
+                MessageBox.Show("Acceso denegado. Revise los parámetros de entrada o regístrese");
+            }
+           // this.BackColor = Color.Gray;
+            //server.Shutdown(SocketShutdown.Both);
+            //server.Close();
+        }
+
+        private void Entrar_Load(object sender, EventArgs e)
+        {
+            label3.Hide();
+            textBox3.Hide();
+            label4.Hide();
+            textBox4.Hide();
+            button4.Hide();
+            button6.Hide();
+            button5.Hide();
+            button7.Hide();
+            button8.Hide();
+            
+            
+            textBox5.Hide();
+
+            radioButton1.Hide();
+            radioButton2.Hide();
+            radioButton3.Hide();
+            button5.Hide();
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string mensaje = "2/" + textBox3.Text + "/" + textBox4.Text + "";
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+            if (mensaje == "SI")
+            {
+                MessageBox.Show("Registrado correctamente");
+               
+
+            }
+            else if (mensaje == "NO")
+            {
+                MessageBox.Show("No se ha podido registrar");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Show();
+            textBox1.Show();
+            label2.Show();
+            textBox2.Show();
+            button2.Show();
+            button1.Show();
+
+            label3.Hide();
+            textBox3.Hide();
+            label4.Hide();
+            textBox4.Hide();
+            button4.Hide();
+            button6.Hide();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                string mensaje = "3/" + textBox5.Text;
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+
+
+                MessageBox.Show("El jugador ganador de esta fecha es:" + mensaje);
+               
+
+            }
+            if (radioButton2.Checked)
+            {
+                string mensaje = "4/" + textBox5.Text;
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+
+                if (mensaje == "NO")
+                {
+                    MessageBox.Show("No existe ningún jugador con este nombre");
+
+                }
+                else
+                {
+                    MessageBox.Show("Puntuación total del jugador:" + mensaje);
+                    
+                }
+
+            }
+            if (radioButton3.Checked)
+            {
+                string mensaje = "5/" + textBox5.Text;
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+
+
+                if (mensaje == "NO")
+                {
+                    MessageBox.Show("No existe ningún jugador con este nombre");
+
+                }
+                else
+                {
+                    MessageBox.Show("Fecha y hora:" + mensaje);
+                    
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string mensaje = "7/" + textBox1.Text;
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+
+           
+            
+            
+            
+            textBox5.Hide();
+
+            radioButton1.Hide();
+            radioButton2.Hide();
+            radioButton3.Hide();
+            button7.Hide();
+            button5.Hide();
+            button8.Hide();
+
+            label1.Show();
+            textBox1.Show();
+            label2.Show();
+            textBox2.Show();
+            button2.Show();
+            button1.Show();
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string mensaje = "6/" ;
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+          
+           
+          
+
+
+            MessageBox.Show( "Estos son los usuarios conectados: "+ mensaje);
+
+        }
+
+
+
+
+        
+
+        
+    }
+}
